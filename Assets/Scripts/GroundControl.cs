a490c87ee06dd1b218d38aa5b0ab00c5a38eaf02
mjsacker <mjsacker@gmail.com>
2016-05-26 12:09:39 -0700

    Game

---
+++ Assets/Scripts/GroundControl.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GroundControl : MonoBehaviour {
+	
+		public float speed = 0.5F;
+		public Renderer rend;
+		void Start() {
+			rend = GetComponent<Renderer>();
+		}
+		void Update() {
+			float offset = Time.time * speed;
+			rend.material.mainTextureOffset = new Vector2(0,-offset);
+		}
+}
